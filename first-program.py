2664f7feb3782327c374341dc87cfa2d1bc69887
Janak Gurung <janakgurung@gmail.com>
2025-06-11 08:41:58 +0545

    about while

--- first-program.py
+++ first-program.py
@@ -1,11 +1,14 @@
-# first-program
-try:
-    age= int(input("Enter your age:")) 
-    if age< 15:
-        print(f"{age} is minor")
-    elif age>=15:
-        print(f'{age} is old')
-    else:
-        print("enter a proper age")
-except:
-    print("please enter numbers")
+while True:
+    try:
+        age= int(input("Enter your age:")) 
+        if age< 15:
+            print(f"{age} is minor")
+        elif age>=15:
+            print(f'{age} is old')
+        else:
+            print("enter a proper age")
+    except:
+        print("please enter numbers")
+    run= input("Do you wnat to continue??(y/n)")
+    if run =="n":
+        break
