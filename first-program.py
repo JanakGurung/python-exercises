498e6b806a42271b74bc4f5117965bfd66bca6f4
Janak Gurung <janakgurung@gmail.com>
2025-06-11 08:51:36 +0545

    about for loop

--- first-program.py
+++ first-program.py
@@ -1,14 +1,11 @@
-while True:
+for i in range(2,6,2):
     try:
-        age= int(input("Enter your age:")) 
-        if age< 15:
-            print(f"{age} is minor")
-        elif age>=15:
-            print(f'{age} is old')
-        else:
-            print("enter a proper age")
+            age= int(input("Enter your age:")) 
+            if age< 15:
+                print(f"{age} is minor")
+            elif age>=15:
+                print(f'{age} is old')
+            else:
+                print("enter a proper age")
     except:
         print("please enter numbers")
-    run= input("Do you wnat to continue??(y/n)")
-    if run =="n":
-        break
