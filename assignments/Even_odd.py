16c7c24f06e9450a47ddcb2af5b5bdd7ab8950a5
Janak Gurung <janakgurung@gmail.com>
2025-06-12 08:29:30 +0545

    question 2

---
+++ assignments/Even_odd.py
@@ -0,0 +1,6 @@
+#Write a program that asks the user a number and identify whether it is even or odd.
+age= int(input("Enter a number."))
+if age % 2 == 0:
+    print("Age is even")
+else:
+    print("Age is odd")
