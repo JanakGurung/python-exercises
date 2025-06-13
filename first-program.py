68adb99c34b4f546529f488956556b6e63f08a9c
Janak Gurung <janakgurung@gmail.com>
2025-06-13 08:38:07 +0545

    adding value

--- first-program.py
+++ first-program.py
@@ -3,8 +3,9 @@ my_list=["ankit","janak","sunil","ankit"]
 my_tuple=("ankit","janak","sunil")
 my_set={"ankit","janak","sunil"}
 my_dict={"ankit":1,"janak":2,"sunil":3,"ankit":3}
-print(my_list[3])
-print(my_tuple[2])
-for i in my_set:
-    print(i)
-print(my_dict["ankit"])
+my_dict["vishnu"]=4
+my_list.append(4)
+my_set.add(4)
+print(my_list)
+print(my_set)
+print(my_dict)
