2e01b531a4862805c996dd0b32b6a49477b7b922
Janak Gurung <janakgurung@gmail.com>
2025-06-13 08:14:15 +0545

    Python Data Structures

--- first-program.py
+++ first-program.py
@@ -1,11 +1,9 @@
-for i in range(2,6,2):
-    try:
-            age= int(input("Enter your age:")) 
-            if age< 15:
-                print(f"{age} is minor")
-            elif age>=15:
-                print(f'{age} is old')
-            else:
-                print("enter a proper age")
-    except:
-        print("please enter numbers")
+#list #tuple #set #dictionary
+my_list=["ankit","janak","sunil"]
+my_tuple=("ankit","janak","sunil")
+my_set={"ankit","janak","sunil"}
+my_dict={"ankit":1,"janak":2,"sunil":3}
+print(my_list)
+print(my_tuple)
+print(my_set)
+print(my_dict)
