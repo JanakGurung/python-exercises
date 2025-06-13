e8fdb26f4257fa234f97497d1a61ce0b6743cc07
Janak Gurung <janakgurung@gmail.com>
2025-06-13 08:27:22 +0545

    access elements

--- first-program.py
+++ first-program.py
@@ -1,9 +1,10 @@
 #list #tuple #set #dictionary
-my_list=["ankit","janak","sunil"]
+my_list=["ankit","janak","sunil","ankit"]
 my_tuple=("ankit","janak","sunil")
 my_set={"ankit","janak","sunil"}
-my_dict={"ankit":1,"janak":2,"sunil":3}
-print(my_list)
-print(my_tuple)
-print(my_set)
-print(my_dict)
+my_dict={"ankit":1,"janak":2,"sunil":3,"ankit":3}
+print(my_list[3])
+print(my_tuple[2])
+for i in my_set:
+    print(i)
+print(my_dict["ankit"])
