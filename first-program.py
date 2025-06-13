5988da46a1f31c8f7a3ddfe4182b4a7c61d95404
Janak Gurung <janakgurung@gmail.com>
2025-06-13 08:47:50 +0545

    changes

--- first-program.py
+++ first-program.py
@@ -4,9 +4,8 @@ my_tuple=("ankit","janak","sunil")
 my_set={"ankit","janak","sunil"}
 my_dict={"ankit":1,"janak":2,"sunil":3,"ankit":3}
 my_dict["vishnu"]=4
-my_list.remove("janak")
-my_set.remove("janak")
-del my_dict['ankit']
+my_list[2]="ankit"
+my_dict['ankit']=15
 print(my_list)
 print(my_set)
 print(my_dict)
