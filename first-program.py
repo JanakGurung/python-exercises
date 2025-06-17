6d94da07088add064a9ed678a078146e5a66e560
Janak Gurung <janakgurung@gmail.com>
2025-06-17 08:18:38 +0545

    Abstraction

--- first-program.py
+++ first-program.py
@@ -12,14 +12,44 @@ class Teacher: #creating a class
     
     def give_assignment(self): #creating the behavior of a class
         print("complete the assignment")
- 
- #creating object of the class       
-teacher1 = Teacher("Sunil", 25) 
+        print("Maintain silence in class")
+
+
+class Math_Teacher(Teacher):
+    def __init__(self, name, age, strict):
+        super().__init__(name, age)
+        self.__strict = strict
+        
+    def set_strict(self, strict):
+        self.__strict = strict
+        
+    def get_strict(self):
+        return self.__strict
+    
+    
+class English_teacher(Teacher):
+    def __init__(self, name, age, polite):
+        super().__init__(name, age)
+        self.__polite = polite
+        
+    def set_polite(self, polite):
+        self.__polite= polite
+    
+    def get_polite(self):
+        return self.__polite
+    
+    
+    
+#creating object of the class       
+Math_Teacher1 = Math_Teacher("Sunil", 25, "Very Strict")
+English_Teacher1= English_teacher("Vishnu", 29, "Very Polite")
 
 # print(teacher1.age)
 # print(teacher1.name)      
 
 #calling the functions of the class
-teacher1.give_assignment()
-teacher1.set_name("Janak")
-print(teacher1.get_name())
+Math_Teacher1.give_assignment()
+English_Teacher1.give_assignment()
+print(English_Teacher1.get_name())
+print(Math_Teacher1.get_name())
+print(Math_Teacher1.get_strict())
